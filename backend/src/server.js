0738be370b2045e83abeaac84633feba5669ffe1
Munira Ibrahim <muniraibrahim144@gmail.com>
2026-02-03 10:05:49 +0100

    feat(api): add endpoint to fetch all applicants
    - Created GET /applicants route that's connected to RecruitementDAO

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+/** @requires module:./src/business/RecruitementController */
+const RecruitementController = require("./business/RecruitementController");
 
 const app = express();
+const recruitementController = new RecruitementController();
 
 app.use(cors({origin: "http://localhost:5173",}));
 
@@ -9,6 +12,20 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello backend");
+});
+
+/**
+ * Route to fetch the applicants
+ * Triggers business logic layer
+ */
+app.get("/applicants", async (req, res) => {
+  try {
+    const applicants = await recruitementController.getAllApplicants();
+    res.json(applicants);
+  } catch (error) {
+    console.error("Error fetching applicants:", error);
+    res.status(500).json({ error: "Internal Server Error: Couldn't fetch applicants" });
+  }
 });
 
 app.listen(3000, () => {
