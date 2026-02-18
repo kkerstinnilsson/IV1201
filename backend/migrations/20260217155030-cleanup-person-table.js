0a2b8305e2eaada765a9c9259d7fe1b0b4f6a6a4
Munira Ibrahim <muniraibrahim144@gmail.com>
2026-02-18 13:49:22 +0100

    feat(database): migrations and dependency sync
    - Created migration to add 'application' and 'credentials' tables
    - Created migration to remove 'username' and 'password' from 'person' table
    - Updated package.json with necessary sequelize dependencies.

---
+++ backend/migrations/20260217155030-cleanup-person-table.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    // Remove 'username' and 'password' columns from 'person' table
+    await queryInterface.removeColumn('person', 'username');
+    await queryInterface.removeColumn('person', 'password');
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * In case of rollback add 'username' and 'password' 
+     * columns back to 'person' table
+     * but the data will be lost 
+     */
+    await queryInterface.addColumn('person', 'username', { 
+      type: Sequelize.STRING,
+      allowNull: true 
+    });
+    await queryInterface.addColumn('person', 'password', { 
+      type: Sequelize.STRING,
+      allowNull: true 
+    });
+  }
+};
