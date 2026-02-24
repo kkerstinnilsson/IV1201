59e1e2f219fe9aae9d7c63a8e02d166adc88dd08
Linnea Johansson <linnea6@kth.se>
2026-02-24 13:43:52 +0100

    fix: hash passwords in RDS database
    - Create script for migrating database to hashed passwords

---
+++ backend/migrations/20260224131752-hash-passwords.js
@@ -0,0 +1,29 @@
+'use strict';
+const bcrypt = require('bcrypt');
+const BCRYPT_ROUNDS = 12;
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const credentials = await queryInterface.sequelize.query(
+      'SELECT credential_id, password FROM credentials',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    );
+
+    for (const credential of credentials) {
+      const isHashed = credential.password.startsWith('$2b$') && credential.password.length === 60;
+
+      if (!isHashed) {
+        const hashed = await bcrypt.hash(credential.password, BCRYPT_ROUNDS);
+
+        await queryInterface.sequelize.query(
+          'UPDATE credentials SET password = ? WHERE credential_id = ?',
+          { replacements: [hashed, credential.credential_id] }
+        );
+      }
+    }
+  },
+  async down(queryInterface, Sequelize) {
+    // Hashing is one-way and cannot be reversed
+  }
+};
