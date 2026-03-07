c654343ad10b694de83646e7478bd47f5d0466be
EzdaXamgin <lorinpalew@gmail.com>
2026-03-07 12:38:38 +0100

    feat(main): add ErrorBoundary and register unauthorized redirect handler

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,19 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import ErrorBoundary from './components/ErrorBoundary.jsx'
+import { setUnauthorizedHandler } from './services/httpClient'
+
+setUnauthorizedHandler(() => {
+  window.location.href = '/login'
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </BrowserRouter>
   </StrictMode>,
 )
